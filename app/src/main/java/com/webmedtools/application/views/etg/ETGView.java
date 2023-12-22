be14c06ef4c4ca15e207274b5175b3f377281dc5
Amrit Menon <amritm@Mac-and-Cheese.local>
2023-12-21 19:36:17 -0600

    made horizontal button layout

--- app/src/main/java/com/webmedtools/application/views/etg/ETGView.java
+++ app/src/main/java/com/webmedtools/application/views/etg/ETGView.java
@@ -2,8 +2,11 @@ package com.webmedtools.application.views.etg;
 
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -13,23 +16,44 @@ import com.webmedtools.application.views.MainLayout;
 @PageTitle("ETG")
 @Route(value = "ETG", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
-public class ETGView extends HorizontalLayout {
+public class ETGView extends VerticalLayout {
 
-    private TextField name;
-    private Button sayHello;
+    Button generate = new Button("Generate");
+    Button download = new Button("Download (PDF)");
+    Button print = new Button("Print");
+    TextArea textArea = new TextArea();
 
     public ETGView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-        sayHello.addClickShortcut(Key.ENTER);
+        textArea.setWidthFull();
+        textArea.setLabel("Preview");
+        textArea.setReadOnly(false);
+        textArea.setAutocapitalize(null);
+        textArea.setAutocomplete(null);
+        textArea.setAutocorrect(false);
+        textArea.setPlaceholder("Click the generate button to see a preview in this text box, \nthen you can either download it as a file or print it.");
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-
-        add(name, sayHello);
+        add(textArea, buttonLayout());
     }
 
+    private HorizontalLayout buttonLayout() {
+        generate.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        generate.addClickListener(click -> {
+            //eService.fillTextArea(textArea);
+            //VaadinSession.getCurrent().setAttribute(TextArea.class, textArea);
+        });
+
+        // Create a download button
+        download.addThemeVariants(ButtonVariant.MATERIAL_CONTAINED);
+
+
+
+        // ***ADD PRINT FUNCTIONALITY***
+        print.addThemeVariants(ButtonVariant.MATERIAL_CONTAINED);
+        print.addClickListener(click -> {
+            // Opens the PrintView page in a new tab
+
+        });
+
+        return new HorizontalLayout(generate, download, print);
+    }
 }
